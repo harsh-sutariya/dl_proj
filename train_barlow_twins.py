d048ad08fb08559defe641341dc9b042a9a7ed27
soham-chitnis10 <sohamchitnis10@gmail.com>
2024-12-10 09:35:52 -0500

    updated device

--- train_barlow_twins.py
+++ train_barlow_twins.py
@@ -62,7 +62,7 @@ def main(args):
                     img3 = location_transform(img2)
                     selected = embed2
                 embed3 = encoder(img3)
-                cos = nn.CosineEmbeddingLoss(margin=args.margin)(selected, embed3, torch.ones(selected.shape[0], device=rank)*-1)
+                cos = nn.CosineEmbeddingLoss(margin=args.margin)(selected, embed3, torch.ones(selected.shape[0], device=device)*-1)
                 corr_matrix = torch.matmul(normalized_embed1.T,normalized_embed2)/embed1.shape[0]
                 c_diff = (corr_matrix - torch.eye(encoder.repr_dim).to(device)).pow(2)
                 c_diff *= (off_diagonal*args.lam + torch.eye(encoder.repr_dim).to(device))
