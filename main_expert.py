6387801129ac6b5a35d2534972000e8addea4016
soham-chitnis10 <sohamchitnis10@gmail.com>
2024-12-14 12:04:51 -0500

    test hidden sets

---
+++ main_expert.py
@@ -0,0 +1,76 @@
+from dataset import create_wall_dataloader
+from evaluator import ProbingEvaluator
+import torch
+from models import MockModel, JEPA, JEPA_RNNCell, JEPA_RNN
+import glob
+import utils
+
+
+def get_device():
+    """Check for GPU availability."""
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+    return device
+
+
+def load_data(device):
+    data_path = "/scratch/DL24FA"
+
+    probe_train_ds = create_wall_dataloader(
+        data_path=f"{data_path}/probe_expert/train",
+        probing=True,
+        device=device,
+        train=True,
+    )
+
+    probe_val_normal_ds = create_wall_dataloader(
+        data_path=f"{data_path}/probe_expert/val",
+        probing=True,
+        device=device,
+        train=False,
+    )
+
+    probe_val_wall_ds = create_wall_dataloader(
+        data_path=f"{data_path}/probe_wall_other/val",
+        probing=True,
+        device=device,
+        train=False,
+    )
+
+    probe_val_ds = {"normal": probe_val_normal_ds, "wall": probe_val_wall_ds}
+
+    return probe_train_ds, probe_val_ds
+
+
+def load_model():
+    """Load or initialize the model."""
+    # TODO: Replace MockModel with your trained model
+    model = JEPA_RNNCell()
+    model.load_state_dict(torch.load("./jepa.pth"))
+    model.to(get_device())
+    utils.freeze_param(model)
+    return model
+
+
+def evaluate_model(device, model, probe_train_ds, probe_val_ds):
+    evaluator = ProbingEvaluator(
+        device=device,
+        model=model,
+        probe_train_ds=probe_train_ds,
+        probe_val_ds=probe_val_ds,
+        quick_debug=False,
+    )
+
+    prober = evaluator.train_pred_prober()
+
+    avg_losses = evaluator.evaluate_all(prober=prober)
+
+    for probe_attr, loss in avg_losses.items():
+        print(f"{probe_attr} loss: {loss}")
+
+
+if __name__ == "__main__":
+    device = get_device()
+    probe_train_ds, probe_val_ds = load_data(device)
+    model = load_model()
+    evaluate_model(device, model, probe_train_ds, probe_val_ds)
